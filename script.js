c12d61fb37805bcf4a99a2899196f872b0107991
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-13 00:53:00 -0800

    changing the background color of my html page using my style.css and I also used a bootstrap css to assist me with decorating my html page

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ var time = 100
 var quizScore = 100
 var highestScore = 0
 var quizTime;
-var questionsAsked = [
-    {
-        titled: "How many States are there in the United States?",
-        Choices: ["Fifty", "Forty", "None"],
-        Answer: "Fifty"
-    },
-
-]
+var questionsAsked =
 
 
