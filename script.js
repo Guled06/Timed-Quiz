9ebd4bd8b16f89da07ed5a78b0d1c16c0ed48562
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-13 00:42:05 -0800

    added intial files to the project

---
+++ script.js
@@ -0,0 +1,14 @@
+var time = 100
+var quizScore = 100
+var highestScore = 0
+var quizTime;
+var questionsAsked = [
+    {
+        titled: "How many States are there in the United States?",
+        Choices: ["Fifty", "Forty", "None"],
+        Answer: "Fifty"
+    },
+
+]
+
+
